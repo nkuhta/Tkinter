82e06baabd7535958799cbaa3d1843a4efc80e51
nkuhta <nicholas.kuhta@gmail.com>
2018-04-19 21:27:49 -0700

    first gui

---
+++ First_GUI.py
@@ -0,0 +1,33 @@
+
+from tkinter import *
+
+# initialze root tkinter object
+root = Tk()
+
+
+############################################################
+#################   Adding Labels   ########################
+############################################################
+
+#  Add a label
+# amazing_label = Label(root,text='Amazing Label!')
+# #  show the label
+# amazing_label.pack()
+#
+# label_2 = Label(root,text='This is alright... Label 2')
+# label_2.pack()
+
+############################################################
+#################   Geometric Managers   ###################
+############################################################
+ # pack = easiest
+ # grid = more detail
+ # place = even more control
+
+############################################################
+#################        Buttons        ####################
+############################################################
+
+
+
+root.mainloop()
